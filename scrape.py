5d938549cff8e192a672ebb99503822e329d0d07
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-21 01:53:12 -0400

    Get all weekly grosses with dates

--- scrape.py
+++ scrape.py
@@ -5,13 +5,28 @@ import json
 BASE_URL = "http://www.boxofficemojo.com"
 FRANCHISE_URL = "/franchises/chart/?id=avengers.htm"
 
-def get_movie_links(url):
+def get_soup(url):
     html = urlopen(BASE_URL + url).read()
     soup = BeautifulSoup(html, "lxml")
+    return soup
+
+def get_movie_links(soup):
     worldwide = soup.find(id="body").find_all("table")[2].find_all("table")[3]
-    return [{"link" : link.get("href"),
+    return [{"link" : link.get("href") + "&page=weekly",
              "title": link.text}
              for link in worldwide.find_all("a")]
 
+def get_weekly_gross(soup):
+    weekly_rows = soup.find("table", "chart-wide").find_all("tr")
+    weekly_rows.pop(0)
+    return [{"date" : data.find_all("td")[0].text,
+             "gross": data.find_all("td")[2].text}
+            for data in weekly_rows]
 
-print json.dumps(get_movie_links(FRANCHISE_URL))
+def get_all_weekly(soup):
+    for data in get_movie_links(soup):
+        url = data.get("link")
+        title = data.get("title")
+        print get_weekly_gross(get_soup(url))
+
+print get_all_weekly(get_soup(FRANCHISE_URL))
