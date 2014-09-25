5d4fede6564e3c2dd23da335fa06fd9e07971c21
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-09-25 01:43:48 -0400

    Adding scraping for country wise total gross

--- scrape.py
+++ scrape.py
@@ -13,9 +13,10 @@ def get_soup(url):
 
 def get_movie_links(soup):
     worldwide = soup.find(id="body").find_all("table")[2].find_all("table")[3]
-    return [{"link" : link.get("href") + "&page=weekly",
+    return [{"link" : link.get("href"),
              "title": link.text}
              for link in worldwide.find_all("a")]
+
 
 def get_weekly_gross(soup, title):
     weekly_rows = soup.find("table", "chart-wide").find_all("tr")
@@ -26,16 +27,36 @@ def get_weekly_gross(soup, title):
             for data in weekly_rows]
 
 def get_all_weekly(soup):
-    results = [get_weekly_gross(get_soup(data.get("link")), data.get("title"))
+    results = [get_weekly_gross(get_soup(data.get("link")+"&page=weekly"), data.get("title"))
                 for data in get_movie_links(soup)]
     return [val for result in results for val in result]
 
-def save_json():
-    json_data = json.dumps(get_all_weekly(get_soup(FRANCHISE_URL)))
+def get_country_gross(soup, title):
+    try:
+        country_rows = soup.find(id="body").find_all("table")[4].find("table").find_all("tr")
+        [country_rows.pop(0) for i in range(3)]
+        return [{"title": title,
+                 "country": data.find_all("td")[0].text,
+                 "gross" : data.find_all("td")[5].text} 
+                 for data in country_rows]
+    except Exception, e:
+        pass
+    
+def get_all_country(soup):
+    results = [get_country_gross(get_soup(data.get("link")+"&page=intl"), data.get("title"))
+                for data in get_movie_links(soup)]
+    results = filter(None, results)
+    return [val for result in results for val in result]
+    
+def save_json(flag):
+    if flag == 0:
+        json_data = json.dumps(get_all_weekly(get_soup(FRANCHISE_URL)))
+    elif flag == 1:
+        json_data = json.dumps(get_all_country(get_soup(FRANCHISE_URL)))
     f = open("data.json", "w+")
     f.write(json_data)
     f.close()
     print "Save to json file data.json"
 
 if __name__ == "__main__":
-    save_json()
+    save_json(1)
